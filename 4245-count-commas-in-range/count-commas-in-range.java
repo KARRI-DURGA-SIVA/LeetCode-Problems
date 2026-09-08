f5b8d87fcb4ce6360303139f12ceecaecbc81dd5
KARRI DURGA SIVA <durgasivayyakarri@gmail.com>
2026-09-08 22:36:36 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,11 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-
-        if (n >= 1000) {
-            count = n - 999;
+        if (n < 1000) {
+            return 0;
         }
-
-        return count;
+        return n - 999;
     }
 }
